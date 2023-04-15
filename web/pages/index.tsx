26d6c12992892fb22fde9e5fa291c434da54e21b
Michael Rickert <michael.rickert@sorare.com>
2023-04-15 15:19:45 -0400

    add basic FE (next.js + chakra ui) scaffolding

---
+++ web/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button, Text } from "@chakra-ui/react";
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <Button>
+      <Text fontWeight="bold">Hello World</Text>
+    </Button>
+  );
+};
+
+export default HelloWorld;
