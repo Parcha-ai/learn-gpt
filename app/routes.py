81e96d5064612da0a96e978cb220547b0511365b
David Byttow <david@sorare.com>
2023-04-15 20:28:27 -0400

    add quart schema

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,17 @@
 import os
 import json
 
-from typing import Dict, List
+from pydantic import BaseModel
+from typing import Dict, List, Optional
 from quart import Quart, request, jsonify
+from quart_schema import QuartSchema, validate_request, validate_response
 
 from app import store
 from app.learn import create_plan, fake_plan
+from app.model import Plan
 
 app = Quart(__name__)
+QuartSchema(app)
 
 DEFAULT_MODEL = "gpt-3.5-turbo"
 DEFAULT_OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
@@ -17,32 +21,47 @@ def bad_request(message: str):
     return jsonify({"error": message}), 400
 
 
+class CreatePlanRequest(BaseModel):
+    goal: str
+    model: Optional[str]
+    openai_api_key: Optional[str]
+
+
+class CreatePlanResponse(BaseModel):
+    plan: Plan
+
+
 @app.route("/v1/create_plan", methods=["POST"])
-async def create_plan():
-    data = await request.json
-    if data.get("fake", False):
-        return jsonify({"plan": json.loads(fake_plan().json())})
-    goal = data.get("goal")
-    if not goal:
-        return bad_request("goal not provided")
-    openai_api_key = data.get("openai_api_key", DEFAULT_OPENAI_API_KEY)
-    model = data.get("model", DEFAULT_MODEL)
+@validate_request(CreatePlanRequest)
+@validate_response(CreatePlanResponse)
+async def create_plan(data: CreatePlanRequest):
     plan = create_plan(
-        goal=goal, model=model, openai_api_key=openai_api_key, verbose=True
+        goal=data.goal,
+        model=data.model or DEFAULT_MODEL,
+        openai_api_key=data.openai_api_key or DEFAULT_OPENAI_API_KEY,
+        verbose=True,
     )
     return jsonify({"plan": json.loads(plan.json())})
 
 
+class GetPlanRequest(BaseModel):
+    id: str
+
+
+class GetPlanResponse(BaseModel):
+    plan: Plan
+
+
 @app.route("/v1/get_plan", methods=["POST"])
-async def get_plan():
-    data = await request.json
-    if data.get("fake", False):
+@validate_request(GetPlanRequest)
+@validate_response(GetPlanResponse)
+async def get_plan(data: GetPlanRequest):
+    # data = await request.json
+    if data.id == "fake":
         return jsonify({"plan": json.loads(fake_plan().json())})
-    id = data.get("id")
-    if not id:
-        return bad_request("goal not provided")
-    plan = store.get_plan(id)
-    return jsonify({"plan": json.loads(plan.json())})
+    plan = store.get_plan(data.id)
+    return GetPlanResponse(plan=plan)
+    # return jsonify({"plan": json.loads(plan.json())})
 
 
 @app.after_request
